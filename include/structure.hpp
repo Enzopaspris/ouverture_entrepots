8d530fda34425c42bad7a55266e67d53bc1316cd
enzo <etud@port-hp-probook650G8-7>
2025-10-06 13:52:12 +0200

    test

--- include/structure.hpp
+++ include/structure.hpp
@@ -1,11 +1,14 @@
 #pragma once
 #include <vector>
+using namespace std;
 
 struct Entrepot {
     unsigned int cout_ouv;
-    std::vector<int> cou_app;
+    vector<int> cout_app;
     unsigned int capa_max;
     bool est_ouvert = false;
+
+
 };
 
 struct Magasin {
