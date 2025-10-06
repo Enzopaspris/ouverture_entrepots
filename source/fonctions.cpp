b70e1e47232c98f2590932b509280e158ab89669
etud user <etud@port-hp-probook650G8-2>
2025-10-06 12:49:14 +0200

    modif main include

--- source/fonctions.cpp
+++ source/fonctions.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 
-#include "../lib/structure.hpp"
+#include "../include/structure.hpp"
 
 
--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "fonctions.cpp"
 using namespace std;
 
 
