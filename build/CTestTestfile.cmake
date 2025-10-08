38e80e3589cb1f2c2550e8a0eec5805ee84758da
c'est moi <90576415+Enzopaspris@users.noreply.github.com>
2025-10-08 10:02:33 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/etud/Documents/LP INFO/PROJET_1/git/ouverture_entrepots
-# Build directory: /home/etud/Documents/LP INFO/PROJET_1/git/ouverture_entrepots/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(run "/home/etud/Documents/LP INFO/PROJET_1/git/ouverture_entrepots/build/GestionEntrepots")
-set_tests_properties(run PROPERTIES  _BACKTRACE_TRIPLES "/home/etud/Documents/LP INFO/PROJET_1/git/ouverture_entrepots/CMakeLists.txt;54;add_test;/home/etud/Documents/LP INFO/PROJET_1/git/ouverture_entrepots/CMakeLists.txt;0;")
