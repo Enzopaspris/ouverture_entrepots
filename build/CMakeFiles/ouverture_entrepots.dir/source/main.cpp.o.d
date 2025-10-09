f0e84d822e30f0088c9da8f471187d785b800ec9
c'est moi <90576415+Enzopaspris@users.noreply.github.com>
2025-10-09 10:32:48 +0200

    Delete build directory

--- build/CMakeFiles/ouverture_entrepots.dir/source/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/ouverture_entrepots.dir/source/main.cpp.o: \
- /home/etud/Documents/LP\ INFO/PROJET_1/git/ouverture_entrepots/source/main.cpp \
- /usr/include/stdc-predef.h \
- /home/etud/Documents/LP\ INFO/PROJET_1/git/ouverture_entrepots/source/../include/fonction_menu.hpp
